605c40cd5dd2158ea48885af37d3620e632a7576
qriscilla <pri.whang@gmail.com>
2020-03-21 17:36:45 -0400

    Update state based on input

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import AnimeCropped from '../images/AnimeCropped.png';
 
-
 class Circle extends Component {
     render() {
         return (
             <div className='circle'>
                 <svg height="670" width="670">
                     <circle cx="350" cy="350" r="300" fill='none' stroke='white' strokeWidth='40' />
-                    <circle className="progress__value" cx='350' cy='350' r='300' fill='none' stroke='black' strokeWidth='40' />
+                    <circle 
+                        style={{
+                            strokeDasharray: '1900',
+                            strokeDashoffset: '0',
+                            animation: 'progress 30s'
+                        }} 
+                        cx='350' cy='350' r='300' fill='none' 
+                        stroke='black' strokeWidth='40' 
+                    />
+                    {/* <circle className="progress__value" cx='350' cy='350' r='300' fill='none' stroke='black' strokeWidth='40' /> */}
                     <image className='img-circle' xlinkHref={AnimeCropped} x='70' y="15" height="670" width="670" />
                 </svg>
             </div>
--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -1,26 +1,44 @@
 import React, {Component} from 'react';
 
 class Configuration extends Component {
+    state = {
+        seconds: ''
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
     render() {
         return (
             <div className='configuration'>
-            <h5 style={{fontWeight: 'bold'}}>configuration</h5>
-            <div className='configure'>
-                <span className='configure-left'># sessions</span>
-                <span className='configure-right'><input type='number'></input></span>
-            </div>
-            <div className='configure'>
-                <span className='configure-left'>sessions</span>
-                <span className='configure-right'><input type='number'></input></span>
-            </div>
-            <div className='configure'>
-                <span className='configure-left'>short break</span>
-                <span className='configure-right'><input type='number'></input></span>
-            </div>
-            <div className='configure'>
-                <span className='configure-left'>long break</span>
-                <span className='configure-right'><input type='number'></input></span>
-            </div>   
+                <h5 style={{fontWeight: 'bold'}}>configuration</h5>
+                <div className='configure'>
+                    <span className='configure-left'># sessions</span>
+                    <span className='configure-right'><input type='number'></input></span>
+                </div>
+                <div className='configure'>
+                    <span className='configure-left'>session</span>
+                    <span className='configure-right'>
+                        <input 
+                            type='number'
+                            name='seconds'
+                            value={this.state.seconds}
+                            onChange={this.onChange}
+                        >
+                        </input>
+                    </span>                
+                </div>
+                <input
+                    type='submit'
+                    value='Submit'
+                />
+                <div className='configure'>
+                    <span className='configure-left'>short break</span>
+                    <span className='configure-right'><input type='number'></input></span>
+                </div>
+                <div className='configure'>
+                    <span className='configure-left'>long break</span>
+                    <span className='configure-right'><input type='number'></input></span>
+                </div>  
             </div>
         );
     }
