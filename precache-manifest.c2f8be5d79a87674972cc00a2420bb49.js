aada53c98a1e91297c10d3c9c6692b93a36fb203
qriscilla <pri.whang@gmail.com>
2020-04-04 20:35:07 -0400

    Updates

---
+++ precache-manifest.c2f8be5d79a87674972cc00a2420bb49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2194d3d207f38ea3ebcbae10e7cc8dbf",
+    "url": "/tomodoro/index.html"
+  },
+  {
+    "revision": "e8005e18c6190ee51bf1",
+    "url": "/tomodoro/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5680105de6e66f4772a1",
+    "url": "/tomodoro/static/css/main.5aa222e8.chunk.css"
+  },
+  {
+    "revision": "e8005e18c6190ee51bf1",
+    "url": "/tomodoro/static/js/2.4a6df044.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tomodoro/static/js/2.4a6df044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5680105de6e66f4772a1",
+    "url": "/tomodoro/static/js/main.2e996801.chunk.js"
+  },
+  {
+    "revision": "a89706d9badf8afc8734",
+    "url": "/tomodoro/static/js/runtime-main.87872264.js"
+  },
+  {
+    "revision": "e7706f73eacba96b330d50e52ed8c1a9",
+    "url": "/tomodoro/static/media/AnimeCropped.e7706f73.png"
+  },
+  {
+    "revision": "c98859b05529436cdb93e78c961a62f2",
+    "url": "/tomodoro/static/media/Tomato.c98859b0.png"
+  }
+]);
